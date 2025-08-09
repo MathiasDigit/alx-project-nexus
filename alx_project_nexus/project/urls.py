f8724406a2056f0bcf1d56cd5302d74470915756
MathiasDigit <bellmathias33@gmail.com>
2025-08-09 03:55:14 +0100

    Update urls.py

--- alx_project_nexus/project/urls.py
+++ alx_project_nexus/project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import routers, permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.http import HttpResponse
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -17,10 +18,12 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+def home_view(request):
+    return HttpResponse("Welcome to the ALX_PROJECT_NEXUS API 🚀")
+
 #ici nous creons notre routeur
 router = routers.SimpleRouter()
-#puis nous declarions une url basee sur le mot cle 'category' et notre view
-#afin que url generee soit celle que nous souhaitons 'api/category/'
+
 router.register('category', CategoryViewset, basename='category')
 router.register('product', ProductViewset, basename='product')
 router.register('article', ArticleViewset, basename='article')
@@ -29,11 +32,13 @@ router.register('admin/product', AdminProductViewset, basename='admin-product')
 router.register('admin/article', AdminArticleViewset, basename='admin-article')
 
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/auth/', include('shop.urls'))
 ]
